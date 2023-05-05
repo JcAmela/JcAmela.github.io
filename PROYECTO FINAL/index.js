d425425c2387d1c19915b12d006a837ad7805182
JcAmela <juancarlosamela@gmail.com>
2023-05-05 15:01:09 +0200

    actualizacion

--- PROYECTO FINAL/index.js
+++ PROYECTO FINAL/index.js
@@ -1,102 +1,157 @@
-window.onload=inici;
-let imagenes = ["chupachups.png","fotografia.png","gorra.png","llavero.png","maquineta.png","tenedor_lapiz.png"];
-let coin = Math.floor(Math.random()*10)+10;
-let azar=[];
+// Asignar la función inici() al evento "onload" de la ventana del navegador
+window.onload = inici;
 
+// Crear un array con las rutas de las imágenes que se mostrarán en las ventanas de la tragaperras
+let imagenes = ["css.png", "javascript.png", "vue.png", "python.png", "html.png"];
 
-function inici(){  
-    document.querySelector("#lanzar").onclick = tirar;
-    document.querySelector("#cruz").onclick = cerrar;
+// Generar un número aleatorio entre 10 y 20 y asignarlo a la variable "coin"
+let coin = Math.floor(Math.random() * 10) + 10;
+
+// Crear un array vacío para almacenar los índices aleatorios de las imágenes
+let azar = [];
+
+// Crear una variable booleana para controlar si se ha pulsado el botón "lanzar"
+let boleanoTirar = false;
+
+function inici() {
+    // Asignar la función tirar() al evento "onclick" del botón "lanzar"
+    document.getElementById("lanzar").onclick = tirar;
+    document.getElementById("cruz").onclick = cerrar;
     botones();
-    credito();  
-    mp3=document.getElementById("sonido");
-    sonido();
-};
-function sonido(){
-    document.getElementById("boton1").onclick=sonar1;
-    document.getElementById("boton2").onclick=sonar2;
-    document.querySelectorAll("body").insertAdjacentHTML=("beforeend",`<button id="boton1">Escuchar sonido 1</button><br><button id="boton2">Escuchar sonido 2</button>`)
+    credito();
 }
+// Crear un elemento "audio" y asignarlo a la variable "mp3"
+let mp3 = document.createElement("audio");
 
-function sonar1(){
-    mp3.src="audios/ganar.mp3";
-    mp3.play();
+function sonido() {
+    document.getElementById("boton1").onclick = sonarGanar;
+    document.getElementById("boton2").onclick = sonarFinal;
+    document.querySelectorAll("body").insertAdjacentHTML = ("beforeend", `<button id="boton1">Escuchar sonido 1</button><br><button id="boton2">Escuchar sonido 2</button>`)
+    document.getElementById("boton3").onclick = sonarLanzar;
+}
+function sonarGanar() {
+    // Asignar la ruta del archivo de audio "ganar.mp3" al atributo "src" del elemento "audio"
+    document.getElementById("sonido").src = "audios/ganar.mp3";
+
+    // Reproducir el archivo de audio
+    document.getElementById("sonido").play();
 };
-function sonar2(){
-    mp3.src="audios/final.mp3";
-    mp3.play();
+
+function sonarFinal() {
+    // Asignar la ruta del archivo de audio "final.mp3" al atributo "src" del elemento "audio"
+    document.getElementById("sonido").src = "audios/final.mp3";
+
+    // Reproducir el archivo de audio
+    document.getElementById("sonido").play();
 };
-function tirar(){
-    if (coin<=0){
+function sonarLanzar() {  //Añadida la función para reproducir el archivo lanzar.mp3 
+
+    //Asigna la ruta del archivo de audio lanzar.mp3 al atributo src del elemento audio 
+
+    document.getElementById("sonido").src = "audios/lanzar.mp3";
+
+    //Reproduce el archivo de audio 
+
+    document.getElementById("sonido").play();
+}
+function tirar() {
+    sonarLanzar()
+    boleanoTirar = true
+    if (coin <= 0) {
         sinPasta();
-    } else{ 
+    } else {
         coin--;
-        for (let i = 0; i<3; i++){
+        for (let i = 0; i < 3; i++) {
             cambiarImagen(i);
         }
     }
     comprobar();
     credito();
 };
-function cambiarImagen(k){
-    numero= Math.floor(Math.random()*imagenes.length);
-    azar[k]=numero;
-    document.querySelectorAll(".ventana img")[k].src=`img/${imagenes[numero]}`;
-    ;
+
+function cambiarImagen(k) {
+
+    azar[k] = Math.floor(Math.random() * imagenes.length);
+    document.querySelectorAll(".ventana img")[k].src = `img/${imagenes[azar[k]]}`;
 }
-function comprobar(){
-   if (azar[0]==azar[1]&& azar[1] == azar[2]){
-    flex();
-   }
-}
-function sinPasta(){
-    document.querySelector("#velo").style.display="flex";
-    document.querySelector("#mensaje").innerHTML="!! Ha robar carteras !! <BR> !MUERTO HAMBRE!"
-    sonar2();
+function comprobar() {
+    if (azar.every(val => val === azar[0])) {
+        flex();
+    }
 }
 
-function flex(){
-    document.querySelector("#velo").style.display="flex";
-    document.querySelector("#mensaje").innerHTML="!FELICIDADES!<BR> !Has ganado el premio del colacao!";
+function sinPasta() {
+    if (coin <= 0) {
+        document.querySelector("#velo").style.display = "flex";
+        document.querySelector("#mensaje").innerHTML = "¿Quieres volver a jugar?";
+        sonarFinal();
+        let botonReiniciar = document.createElement("button");
+        botonReiniciar.id = "reiniciar";
+        botonReiniciar.innerHTML = "Reiniciar juego";
+        botonReiniciar.onclick = function () {
+            // Reiniciar el juego
+            for (let i = 0; i < 3; i++) {
+                cambiarImagen(i);
+            }
+            boleanoTirar = false
+            coin = Math.floor(Math.random() * 10) + 10;
+            credito(); // Actualizar la interfaz de usuario para mostrar el nuevo crédito
+            document.querySelector("#velo").style.display = "none"; // Ocultar el mensaje de fin de juego
+            document.querySelector("#mensaje").innerHTML = ""; // Vaciar el contenido del mensaje
+            mensaje
+            botonReiniciar.parentNode.removeChild(botonReiniciar); // Eliminar el botón "Reiniciar juego"
+        }
+        document.querySelector("#velo").appendChild(botonReiniciar);
+
+    }
+}
+
+
+function flex() {
+    document.querySelector("#velo").style.display = "flex";
+    document.querySelector("#mensaje").innerHTML = "!FELICIDADES!<BR> !Has ganado mas monedas!";
     premio();
 }
-function cerrar(){
-    document.querySelector("#velo").style.display="none";
-    document.querySelector("#mensaje").innerHTML="";
+function cerrar() {
+    document.querySelector("#velo").style.display = "none";
+    document.querySelector("#mensaje").innerHTML = "";
 }
-function credito(){
-    document.querySelector("#monedas").innerHTML="";
-    document.querySelector("#dinero").innerHTML=`<span>${coin}</span><span class="euros">€</span></div></div>`;
-    for (let p = 0; p<coin; p++){
-        document.querySelector("#monedas").innerHTML+=`<img src="img/moneda.png">`;
+function credito() {
+    document.querySelector("#monedas").innerHTML = "";
+    document.querySelector("#dinero").innerHTML = `<span>${coin}</span><span class="euros">€</span></div></div>`;
+    for (let p = 0; p < coin; p++) {
+        document.querySelector("#monedas").innerHTML += `<img src="img/bitcoin.png">`;
     }
 }
-function premio(){   
-    let random5=Math.floor(Math.random()*5+1)+coin;
-    coin=random5;
+function premio() {
+    boleanoTirar = false
+    let random5 = Math.floor(Math.random() * 5 + 1) + coin;
+    coin = random5;
     credito();
-    sonar1()
+    sonarGanar()
 }
-function botones(){
-let cuantos= document.querySelectorAll(".boton").length;
-    for (let i = 0; i<cuantos; i++){
-        document.querySelectorAll(".boton")[i].onclick=saber;
+function botones() {
+    let cuantos = document.querySelectorAll(".boton").length;
+    for (let i = 0; i < cuantos; i++) {
+        document.querySelectorAll(".boton")[i].onclick = saber;
     }
 }
 
-function saber(){
-    let hijos=this.parentNode.children;
-    for (let u=0; u<hijos.length; u++){
-        if(this==hijos[u] ){
-            if (coin<=0){
-                sinPasta();
-            } else{
-                coin--;
-            cambiarImagen(u)
-            comprobar();
-            credito();
-            break;
+function saber() {
+    if (boleanoTirar == true) {
+        let hijos = this.parentNode.children;
+        for (let u = 0; u < hijos.length; u++) {
+            if (this == hijos[u]) {
+                if (coin <= 0) {
+                    sinPasta();
+                } else {
+                    coin--;
+                    cambiarImagen(u)
+                    comprobar();
+                    credito();
+                    break;
+                }
             }
-        }    
+        }
     }
 }
